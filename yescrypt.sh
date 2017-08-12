ea2bd1605bde751cfed5cf6b14cbe8b3871b0b82
lumastico <lucas.lumastico@gmail.com>
2017-08-12 18:34:16 -0300

    Update yescrypt.sh

--- yescrypt.sh
+++ yescrypt.sh
@@ -1,1 +1,1 @@
-nohup ./minerd -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u 3Ms6QGgPaXMHx77TrbCKurXPVsBSHbp5Ub
+nohup ./minerd -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u 3Ms6QGgPaXMHx77TrbCKurXPVsBSHbp5Ub &
